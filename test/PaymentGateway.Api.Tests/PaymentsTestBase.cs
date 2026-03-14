187d5ad62c963a1095cb387b042f38bb1130ceca
enrico23 <enrico2371@gmail.com>
2026-03-14 12:24:46 +0000

    Add basic payment processing endpoint

---
+++ test/PaymentGateway.Api.Tests/PaymentsTestBase.cs
@@ -0,0 +1,21 @@
+﻿namespace PaymentGateway.Api.Tests
+{
+    public abstract class PaymentsTestBase
+    {
+        protected readonly Random _random = new();
+
+        protected HttpClient CreateClient(InMemoryPaymentStore store)
+        {
+            var factory = new WebApplicationFactory<Program>();
+
+            return factory.WithWebHostBuilder(builder =>
+                builder.ConfigureServices(services =>
+                {
+                    services.AddSingleton(store);
+                    services.AddScoped<IPaymentsRepository, PaymentsRepository>();
+                    services.AddScoped<IPaymentService, PaymentService>();
+                }))
+                .CreateClient();
+        }
+    }
+}
